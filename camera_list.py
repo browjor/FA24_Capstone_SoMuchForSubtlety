2f33eafe8bddf7bd5cee41512cb75e340a47efe2
John Brown <104334868+browjor@users.noreply.github.com>
2024-09-11 15:06:27 -0400

    Create camera_list.py

---
+++ camera_list.py
@@ -0,0 +1,16 @@
+#cams starting at 1001 don't have "cam" in the title
+CameraList = [("https://www.trimarc.org/images/milestone/CCTV_05_65_1362.jpg", 128),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1356.jpg", 4),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1353.jpg", 1001),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1344.jpg", 3),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1337.jpg", 1002),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1330.jpg", 1003),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1327.jpg", 1004),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1317.jpg", 1005),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1313.jpg", 1006),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1307.jpg", 1007),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1297.jpg", 1008),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1283.jpg", 1009),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1267.jpg", 1010),]
+
+
