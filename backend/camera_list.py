60df057a1bc347de3356bd33ae5cd326d34147ca
browjor <browjor@iu.edu>
2024-09-23 22:49:12 -0400

    updated camera list with more cameras and details on each to flesh out what's different between them

--- backend/camera_list.py
+++ backend/camera_list.py
@@ -1,16 +1,31 @@
 #cams starting at 1001 don't have "cam" in the title
-CameraList = [("https://www.trimarc.org/images/milestone/CCTV_05_65_1362.jpg", 128),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1356.jpg", 4),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1353.jpg", 1001),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1344.jpg", 3),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1337.jpg", 1002),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1330.jpg", 1003),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1327.jpg", 1004),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1317.jpg", 1005),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1313.jpg", 1006),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1307.jpg", 1007),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1297.jpg", 1008),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1283.jpg", 1009),
-              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1267.jpg", 1010),]
+CameraList = [("https://www.trimarc.org/images/milestone/CCTV_05_65_1362.jpg", 128, "clear view (south travel on left: I65 2 lanes) (north travel 6 lanes on right: I65 to bridge 2 lanes, to 64 2 lanes,to 71 2 lanes"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1356.jpg", 4, "(south travel on left: I65 3 lanes) (north travel on right: I65 3 lanes) "),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1353.jpg", 1001, "slightly blurry view (uncertain travel: I65 3 lanes) (uncertain travel: I65 3 lanes) "),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1344.jpg", 3, "very blurry view (south travel on right: I65 3 lanes) (north travel on left: I65 3 lanes)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1337.jpg", 1002, "clear view (south travel on right: I65 3 lanes) (north travel on left: I65 3 lanes with 1 on-ramp lane)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1330.jpg", 1003, "clear view with lens flare at night (south travel on left: I65 3 lanes) (north travel on right: I65 3 lanes)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1327.jpg", 1004, " very clear view with cars on road in backround no lens flare  (south travel on left: I65 3 lanes) (north travel on right: I65 3 lanes)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1317.jpg", 1005, " clear view with lens flare at night (south travel on left: I65 3 lanes) (north travel on right: I65 3 lanes)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1313.jpg", 1006, "clear view with lens flare at night (south travel on right: I65 4 lanes, 65 to 264 2 lanes) (north travel on left: I65 3 lanes with on-ramp of 2 lanes)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1307.jpg", 1007, "not available on map, blurry view but no lens flare at night (uncertain travel on bottom going right: 2 sets of separated lanes going one direction) (uncertain travel on top going left: 2 sets of lanes) (two sets of overpass unrelated)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1297.jpg", 1008, "completely blurry"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1283.jpg", 1009, "slightly blurry with lens flare (south travel on left: 4/5 lanes) (north travel on right 4/5 lanes"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1267.jpg", 1010, " clear view with no lens flare at night (north travel on left: I65 5 lanes with 1 onramp) (south travel on right: I65 5 lanes)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_65_1252.jpg", 1011, " clear view with lens flare at night (north travel on left: I65 5 lanes) (south travel on right: I65 5 lanes)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_841_0060.jpg", 1012, " clear view with lens flare at night (west travel on right: 265 2 lanes) (east travel on left: 265 2 lanes)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_265_0117.jpg", 1013, " slightly blurry view with lens flare at night (west travel on right: 265 2 lanes) (east travel on left: 265 2 lanes with 1 lane on-ramp)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_265_0135.jpg", 1014, " slightly blurry view with lens flare at night (east travel on right: 265 2 lanes with on ramp) (west travel on left: 265 2 lanes with 1 lane off-ramp)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_265_0173.jpg", 1015, " slightly blurry view with lens flare at night (east travel on right: 265 2 lanes with on ramp) (west travel on left: 265 2 lanes with 1 lane off-ramp)"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_265_0231.jpg", 81, " very blurry view with lens flare at night slightly obscured (east/north travel on right: 265 2 lanes) (west,south travel on left: 265 2 lanes"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_265_0256.jpg", 1016, " clear view with lens flare at night (north travel on right: 265 2 lanes with construction) (south travel on left: 265 3 lanes"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_265_0268.jpg", 80, " very blurry (north travel on right: 265 3 lanes) (south travel on left: 265 3 lanes"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_265_0325.jpg", 77, " medium blurry (north travel on right: 265 2 lanes) (south travel on left: 265 2 lanes"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_71_0089.jpg", 1017, " clear barely any lens flare (south travel on left: 71 2 lanes) (north travel on right: 71 2 lanes"),
+              ("https://www.trimarc.org/images/milestone/CCTV_05_71_0082.jpg", 1018, " clear but heavy lens flare at night (south travel on right: 71 2 lanes) (north travel on left: 71 2 lanes"),
+
+
+
+              ]
 
 
