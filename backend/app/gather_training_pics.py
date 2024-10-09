3d227105738f24190a08baf50135fdbba3b9154d
browjor <browjor@iu.edu>
2024-10-08 20:20:15 -0400

    gathering more pictures at night

--- backend/app/gather_training_pics.py
+++ backend/app/gather_training_pics.py
@@ -1,14 +1,21 @@
 import time
 import urllib.request
 from backend.app.camera_list import CameraList
+from urllib.error import ContentTooShortError
 from time import localtime
 from dotenv import load_dotenv
 import os
 load_dotenv()
 for i in range(10):
     for camera in CameraList:
-        urllib.request.urlretrieve(camera[0],
-                                   os.getenv("TRAINING_PICS_LOC")
-                                    + str(camera[1]) + "_" + str(i) + "_" + str(localtime().tm_hour) + str(localtime().tm_min) + ".png")
+        try:
+            urllib.request.urlretrieve(camera[0],
+                                       os.getenv("TRAINING_PICS_LOC")
+                                        + str(camera[1]) + "_" + str(i) + "_" + str(localtime().tm_hour) + str(localtime().tm_min) + ".png")
+        except ContentTooShortError as e:
+            print(e,"Error")
+            time.sleep(15)
+            continue
+
         time.sleep(15)
     time.sleep(180)
