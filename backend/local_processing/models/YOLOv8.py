00864718737edd8338bcb3dee31a19fe18f32fd6
Aaron O'Brien <aobpc2000@gmail.com>
2025-02-09 11:01:22 -0500

    Parameterized confidence and save_to_disk

--- backend/local_processing/models/YOLOv8.py
+++ backend/local_processing/models/YOLOv8.py
@@ -2,9 +2,7 @@ from ultralytics import YOLO
 from BoundingBoxes import save_detections_to_disk
 import cv2
 
-confidence = 0.3
 enable_grayscale = True
-save_to_disk = True
 
 def preprocess_image(image):
     preprocessed_image = cv2.imread(image)
@@ -19,21 +17,20 @@ def infer(image, model_weights_path):
     results = model(image)
     return results[0].boxes.data
 
-def extract_vehicle_count(results):
+def extract_vehicle_count(results, confidence):
     vehicles = 0
     for detection in results:
         if detection[4] >= confidence:
             vehicles += 1
     return vehicles
 
-def process_image(image_path, model_weights_path):
+def process_image(image_path, model_weights_path, confidence, save_to_disk):
     image = preprocess_image(image_path)
     detections = infer(image, model_weights_path)
     if save_to_disk:
         save_detections_to_disk(image, image_path, detections, conf=confidence)
-    data = extract_vehicle_count(detections)
+    data = extract_vehicle_count(detections, confidence)
     return data
-
 
 '''
 import os
@@ -43,6 +40,6 @@ images_directory = './NighttimeImages/'
 
 for image_file in os.listdir(images_directory):
     if image_file.endswith('.png'):
-        returned_data = process_image(images_directory + image_file, model_weights)
+        returned_data = process_image(images_directory + image_file, model_weights, 0.3, True)
         print(returned_data)
 '''
