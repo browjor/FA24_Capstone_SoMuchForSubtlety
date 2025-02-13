4c858da7c2f38db6e59771afb668f3bd2a5f549e
Mason Napper <mlnapper@iu.edu>
2025-02-12 19:41:34 -0500

    Adding files for testing

--- trafficlouisville/src/components/Footer/Footer.js
+++ trafficlouisville/src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import Container from 'src/components/Container';
+import React from 'react';
 
 import styles from './Footer.module.scss';
 
--- trafficlouisville/src/components/Map/Map.js
+++ trafficlouisville/src/components/Map/Map.js
@@ -1,22 +1,37 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
+import React from "react";
 
-const DynamicMap = dynamic(() => import('./DynamicMap'), {
-  ssr: false
-});
+// Dynamically import Leaflet components to avoid SSR issues
+const MapContainer = dynamic(() => import("react-leaflet").then(mod => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import("react-leaflet").then(mod => mod.TileLayer), { ssr: false });
+const CircleMarker = dynamic(() => import("react-leaflet").then(mod => mod.CircleMarker), { ssr: false });
 
-// Set default sizing to control aspect ratio which will scale responsively
-// but also help avoid layout shift
+export default function Map({ center, zoom, trafficData = [] }) {
+    // Log trafficData to ensure it's coming through correctly
+    console.log("trafficData:", trafficData);
 
-const DEFAULT_WIDTH = 600;
-const DEFAULT_HEIGHT = 600;
+    // Handle the case where trafficData might not be in the correct format
+    if (!Array.isArray(trafficData)) {
+        console.error("Invalid traffic data format:", trafficData);
+        return <div>Error: Invalid traffic data</div>;
+    }
 
-const Map = (props) => {
-  const { width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT } = props;
-  return (
-    <div style={{ aspectRatio: width / height }}>
-      <DynamicMap {...props} />
-    </div>
-  )
+    return (
+        <div style={{ height: "100vh", width: "100vw" }}>
+            <MapContainer center={center} zoom={zoom} style={{ height: "100%", width: "100%" }}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                {/* Render markers if trafficData is valid */}
+                {trafficData.map(([density, lat, lng], index) => (
+                    <CircleMarker
+                        key={index}
+                        center={[lat, lng]}
+                        radius={Math.max(density / 10, 5)} // Prevents markers from being too small
+                        fillOpacity={0.6}
+                        color="red"
+                    />
+                ))}
+            </MapContainer>
+        </div>
+    );
 }
-
-export default Map;
