3c9654bc9484bb4c17927fcf347d17086aa65d5e
John Brown <104334868+browjor@users.noreply.github.com>
2025-03-26 11:28:31 -0400

    Delete backend/local_processing/models/model_test.py

--- backend/local_processing/models/model_test.py
+++
@@ -1,6 +0,0 @@
-from YOLOv8 import process_image
-
-test = process_image('C:\\Users\\johnb\\PycharmProjects\\FA24_Capstone_SoMuchForSubtlety\\backend\\local_processing\\temp_storage\\31\\current.png','C:\\Users\\johnb\\PycharmProjects\\FA24_Capstone_SoMuchForSubtlety\\backend\\local_processing\\models\\All_Grayscale.pt')
-
-
-print(test)
