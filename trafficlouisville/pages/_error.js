0e38c09f2e59d2d97f1c8ac13c8de73b9a21ae00
Mason Napper <mlnapper@iu.edu>
2025-03-31 13:11:10 -0400

    Added error page corrections

--- trafficlouisville/pages/_error.js
+++ trafficlouisville/pages/_error.js
@@ -1,30 +1,25 @@
-import React from "react";
-
+// pages/_error.js
 function Error({ statusCode }) {
-  if (statusCode >= 500 && statusCode < 600) {
+    if (statusCode === 504) {
+      return (
+        <div style={{ padding: '2rem', textAlign: 'center' }}>
+          <h1>504 Gateway Timeout</h1>
+          <p>Looks like our server is down, try again later :)</p>
+        </div>
+      );
+    }
+    
     return (
-      <div style={{ padding: "40px", textAlign: "center", fontFamily: "sans-serif" }}>
-        <h1>{statusCode} - Server Error</h1>
-        <p>Looks like our server is down, try again later :)</p>
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>{statusCode} Error</h1>
+        <p>An unexpected error has occurred.</p>
       </div>
     );
   }
-
-  return (
-    <div style={{ padding: "40px", textAlign: "center", fontFamily: "sans-serif" }}>
-      <h1>{statusCode} Error</h1>
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : "An error occurred on client"}
-      </p>
-    </div>
-  );
-}
-
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
-};
-
-export default Error;
+  
+  Error.getInitialProps = ({ res, err }) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+    return { statusCode };
+  };
+  
+  export default Error;
